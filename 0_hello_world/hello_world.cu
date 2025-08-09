18bcb2f6b516e875973d7a51bc1ad9a17384ed5d
zhaojianbin <zhaojianbin@synkrotron.ai>
2025-08-09 14:39:44 +0800

    更新helloworld

--- 0_hello_world/hello_world.cu
+++ 0_hello_world/hello_world.cu
@@ -1,12 +1,18 @@
 #include<stdio.h>
+#include <iostream>
+#include <unistd.h>
 __global__ void hello_world(void)
 {
   printf("GPU: Hello world!\n");
+  // std::cout << "Hello GPU" << std::endl;
 }
 int main(int argc,char **argv)
 {
   printf("CPU: Hello world!\n");
-  hello_world<<<1,10>>>();
-  cudaDeviceReset();//if no this line ,it can not output hello world from gpu
+  std::cout << "Hello CPU" << std::endl;
+  hello_world<<<2,10>>>();
+  sleep(5);
+  std::cout << "sleep over " << std::endl; 
+  cudaDeviceReset();//if no this line ,it can not output hello world from gpu 将当前GPU状态恢复到初始状态
   return 0;
 }
